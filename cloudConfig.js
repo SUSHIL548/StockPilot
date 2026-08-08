eb87aca0cc912decdb6545d58e98f27203fb3423
remi-code <sushil@gmail.com>
2026-08-08 20:02:07 +0530

    Debug Cloudinary credentials

--- cloudConfig.js
+++ cloudConfig.js
@@ -10,11 +10,18 @@ console.log("Cloudinary ENV:", {
         : null
 });
 
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
+const testParams = {
+    allowed_formats: "png,jpg,jpeg",
+    folder: "stockpilot_DEV",
+    timestamp: 1786199174,
+};
+
+const testSignature = cloudinary.utils.api_sign_request(
+    testParams,
+    process.env.CLOUD_API_SECRET
+);
+
+console.log("TEST SIGNATURE:", testSignature);
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
